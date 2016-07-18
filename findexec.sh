68bfb514a18a1c59474de1cf704b6adbccb82ec0
brandonocasey <brandonocasey@gmail.com>
2016-07-18 11:56:11 -0400

    small build fix and better cleanup

--- findexec.sh
+++ findexec.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 search="$1"
 cmd="$2"
-dist="$3"
+src="$3"
+if [ -z "$src" ]; then
+	src="src"
+fi
+dist="dist"
 
-find src -name "$search" | while read -r f; do
+find "$src" -name "$search" | while read -r f; do
 	f_dist="$dist/$(dirname "$f" | sed 's~src/~~')"
 	mkdir -p "$f_dist"
 	echo "Running $cmd '$f' > '$f_dist/$(basename "$f")'"
